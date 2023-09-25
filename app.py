e13a28876e93f181aeb2161b5e29f1ac40cdf272
AdamWestcott <72984379+AdamWestcott@users.noreply.github.com>
2023-09-25 23:28:33 +0100

    Changed Data storage and APIs
    Changed Data two distinct dictionarys instead of a list

--- app.py
+++ app.py
@@ -1,55 +1,51 @@
+import uuid
 from flask import Flask, request
-
+from flask_smorest import abort
+from db import items,stores
 app = Flask(__name__)
-
-stores = [
-    {
-        "name":"My store",
-        "items":[
-            {
-                "name":"chair",
-                "price": 15.99
-            }
-        ]
-    }
-]
 
 #Gets All Stores
 @app.get("/store") #http://127.0.0.1:5000/store - this tells the code to run the get_stores function when you access this url
 def get_stores():
-    return {"stores":stores}
+    return {"stores":list(stores.values())}
+
+#Gets All Stores
+@app.get("/item") #http://127.0.0.1:5000/item - this tells the code to run the get_items function when you access this url
+def get_items():
+    return {"items":list(items.values())}
 
 #Creates a Store
 @app.post("/store") #http://127.0.0.1:5000/store - this tells the code to run the create_stores function when you access this url
 def create_stores():
-    request_data = request.get_json()
-    new_store = {"name": request_data["name"], "items":[]}
-    stores.append(new_store)
+    store_data = request.get_json()
+    store_id = uuid.uuid4.hex()
+    new_store = {**store_data, "id":store_id}
+    stores[store_id] = new_store
     return new_store, 201
 
 #Allows you to update a stores items - accepts the store name to access it
-@app.post("/store/<string:name>/item") #http://127.0.0.1:5000/store/<string:name>/item - this tells the code to run the create_item function when you access this url
-def create_Item(name):
-    request_data = request.get_json()
-    for store in stores:
-        if store["name"] == name:
-            new_item = {"name":request_data["name"],"price":request_data["price"]}
-            store["items"].append(new_item)
-            return new_item,201
-    return {"message":"Store Not Found"},404
+@app.post("/item") #http://127.0.0.1:5000/store/<string:name>/item - this tells the code to run the create_item function when you access this url
+def create_Item():
+    item_data = request.get_json()
+    if item_data["store_id"] not in stores:
+        abort(404, message = "Store not found.")
+    item_id = uuid.uuid4.hex()
+    new_item = {**item_data, "id":item_id}
+    items[item_id] = new_item
+    return new_item,201
 
 #Gets A specific Store
-@app.get("/store/<string:name>") #http://127.0.0.1:5000/store/<string:name> - this tells the code to run the get_store function when you access this url
-def get_store(name):
-    for store in stores:
-        if store["name"] == name:
-            return store
-    return {"message":"Store Not Found"},404
+@app.get("/store/<string:store_id>") #http://127.0.0.1:5000/store/<string:name> - this tells the code to run the get_store function when you access this url
+def get_store(store_id):
+    try:
+        return stores[store_id]
+    except KeyError:
+        abort(404, message = "Store not found.")
 
-#Gets the items of a specific Store
-@app.get("/store/<string:name>/item") #http://127.0.0.1:5000/store/<string:name>/items - this tells the code to run the get_store_items function when you access this url
-def get_store_items(name):
-    for store in stores:
-        if store["name"] == name:
-            return {"items":store["items"]}
-    return {"message":"Store Not Found"},404
+#Gets a specific Item
+@app.get("/item/<string:item_id>") #http://127.0.0.1:5000/store/<string:name>/items - this tells the code to run the get_item function when you access this url
+def get_item(item_id):
+    try:
+        return items[item_id]
+    except KeyError:
+        abort(404, message = "Item not found.")
