39d56a5b0f3865b2cf159e0c2afae32ffb6e54fe
AdamWestcott <72984379+AdamWestcott@users.noreply.github.com>
2023-10-03 16:21:50 +0100

    Updated Endpoints
    Updated Endpoints

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,10 @@
 FROM python:3.11
 EXPOSE 5000
 WORKDIR /app
-#installs flask
-RUN pip install flask
+#Copys Requirements.txt into current folder
+COPY  requirements.txt .
+#installs all libaries in requirements.txt
+RUN pip install -r requirements.txt
 COPY . .
 #Runs the flask app, host 0.0.0.0 allows external connections to the docker
 CMD ["flask", "run","--host","0.0.0.0"] 
--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import uuid
 from flask import Flask, request
 from flask_smorest import abort
 from db import items,stores
+
 app = Flask(__name__)
 
 #Gets All Stores
@@ -9,7 +10,7 @@ app = Flask(__name__)
 def get_stores():
     return {"stores":list(stores.values())}
 
-#Gets All Stores
+#Gets All Items
 @app.get("/item") #http://127.0.0.1:5000/item - this tells the code to run the get_items function when you access this url
 def get_items():
     return {"items":list(items.values())}
@@ -30,7 +31,7 @@ def create_stores():
         if store_data["name"] == store["name"]:
             abort(400, message=f"Store already exists.")
     
-    store_id = uuid.uuid4.hex()
+    store_id = uuid.uuid4().hex
     new_store = {**store_data, "id":store_id}
     stores[store_id] = new_store
     return new_store, 201
@@ -63,7 +64,7 @@ def create_Item():
     if item_data["store_id"] not in stores:
         abort(404, message = "Store not found.")
     
-    item_id = uuid.uuid4.hex()
+    item_id = uuid.uuid4().hex
     new_item = {**item_data, "id":item_id}
     items[item_id] = new_item
     return new_item,201
@@ -76,10 +77,54 @@ def get_store(store_id):
     except KeyError:
         abort(404, message = "Store not found.")
 
+#Deletes A Store
+@app.delete("/store/<string:store_id>") #http://127.0.0.1:5000/store/<string:name>/items - this tells the code to run the get_item function when you access this url
+def del_item(store_id):
+    try:
+        del[stores[store_id]]
+        return {"message": "Store has been deleted"}
+    except KeyError:
+        abort(404, message = "Store not found.")
+
+#Updates a Store
+@app.put("/store/<string:store_id>") #http://127.0.0.1:5000/store/<string:name>/items - this tells the code to run the get_item function when you access this url
+def update_item(store_id):
+    store_data = request.get_json()
+    if "price" not in store_data or "name" not in store_data:
+        abort(404, message = "Bad Request. Ensure 'price' and 'name' are included in JSON payload")
+    try:
+        store = stores[store_id]
+        store |= store_data
+        return store
+    except KeyError:
+        abort(404, message = "Item not found.")
+
 #Gets a specific Item
 @app.get("/item/<string:item_id>") #http://127.0.0.1:5000/store/<string:name>/items - this tells the code to run the get_item function when you access this url
 def get_item(item_id):
     try:
         return items[item_id]
+    except KeyError:
+        abort(404, message = "Item not found.")
+
+#Deletes an Item
+@app.delete("/item/<string:item_id>") #http://127.0.0.1:5000/store/<string:item_id> - this tells the code to run the del_item function when you access this url
+def del_item(item_id):
+    try:
+        del[items[item_id]]
+        return {"message": "Item has been deleted"}
+    except KeyError:
+        abort(404, message = "Item not found.")
+
+#Updates an Item
+@app.put("/item/<string:item_id>") #http://127.0.0.1:5000/store/<string:name>/items - this tells the code to run the get_item function when you access this url
+def update_item(item_id):
+    item_data = request.get_json()
+    if "price" not in item_data or "name" not in item_data:
+        abort(404, message = "Bad Request. Ensure 'price' and 'name' are included in JSON payload")
+    try:
+        item = items[item_id]
+        item |= item_data
+        return item
     except KeyError:
         abort(404, message = "Item not found.")
